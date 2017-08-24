64025f192fe6d55eb91a95b5afc90e9b19047128
Anton Khebnev <Khebnev.a.v@gmail.com>
2017-08-24 13:17:22 +0300

    Problem with Handled

--- m2k/m2k/Form1.Designer.cs
+++ m2k/m2k/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(387, 256);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Miles To Kilometers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- m2k/m2k/Form1.cs
+++ m2k/m2k/Form1.cs
@@ -16,5 +16,60 @@ namespace m2k
         {
             InitializeComponent();
         }
+        // нажатие клавиши в поле редактирования
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            /*Правильными символами считаются цифры,
+             * Запятая, <Enter> и <Backspace>.
+             * Будем считать правильным сиволом
+             * также точку, но заменим её запятой.
+             * Остальные символы запрещены.
+             * Чтобы запрещенный символ не отображался
+             * в поле редактирования, присвоим
+             * значение true свойству Handled параметра e
+             */
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                //Цифра
+                return;
+            }
+
+            if (e.KeyChar == '.')
+            {
+                //Точку заменим запятой
+                e.KeyChar = ',';
+            }
+
+            if (e.KeyChar == ',')
+            {
+                if (textBox1.Text.IndexOf('0') != -1)
+                {
+                    //Запятая уже есть в поле редактирования
+                    e.Handled = true;
+                }
+                return;
+            }
+
+            if ( Char.IsControl (e.KeyChar))
+            {
+                //<Enter>, <Backspace>, <Esc>
+                if (e.KeyChar == (char) Keys.Enter)
+                    //Нажата клавига <Enter>
+                    //Установить курсор на кнопку OK
+                    button1.Focus();
+                return;
+            }
+
+            // Остальные символы запрещены
+            e.Handled = true;
+        }
+        //Щелчок по кнопке ОК
+
     }
 }
+
